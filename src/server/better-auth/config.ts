932359a8bb2d63214a1ec08933e5f57b114236f8
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2026-07-15 14:18:10 +0700

    feat: docker build

--- src/server/better-auth/config.ts
+++ src/server/better-auth/config.ts
@@ -16,6 +16,7 @@ export const auth = betterAuth({
     google: {
       clientId: env.BETTER_AUTH_GOOGLE_CLIENT_ID,
       clientSecret: env.BETTER_AUTH_GOOGLE_CLIENT_SECRET,
+      prompt: "select_account",
     },
   },
   account: {
