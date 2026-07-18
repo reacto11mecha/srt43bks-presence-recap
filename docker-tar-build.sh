9ecf801d78b6026702641acbbab7730d51e43f4c
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2026-07-18 13:14:17 +0700

    chore: namechange

--- docker-tar-build.sh
+++ docker-tar-build.sh
@@ -3,10 +3,10 @@
 # Exit immediately if a command exits with a non-zero status
 set -e
 
-echo "📦 Step 1: Building and tagging Docker image [srt43-prescap:latest]..."
-docker build -t "srt43-prescap:latest" .
+echo "📦 Step 1: Building and tagging Docker image [asri:latest]..."
+docker build -t "asri:latest" .
 
-echo "🗜️  Step 2: Saving and compressing image to [srt43-prescap-image.tar] on the fly..."
-docker save "srt43-prescap:latest" | gzip > "srt43-prescap-image.tar"
+echo "🗜️  Step 2: Saving and compressing image to [asri-image.tar] on the fly..."
+docker save "asri:latest" | gzip > "asri-image.tar"
 
-echo "✅ Done! Your image is dumped at: $(pwd)/srt43-prescap-image.tar"
+echo "✅ Done! Your image is dumped at: $(pwd)/asri-image.tar"
