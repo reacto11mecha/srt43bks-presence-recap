5d59da674611a07f4e9821f5631ae2995c1173e4
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2026-07-18 15:11:02 +0700

    feat: menambahakan opentelemetry

--- docker-tar-build.sh
+++ docker-tar-build.sh
@@ -3,10 +3,10 @@
 # Exit immediately if a command exits with a non-zero status
 set -e
 
-echo "📦 Step 1: Building and tagging Docker image [asri:latest]..."
-docker build -t "asri:latest" .
+echo "📦 Step 1: Building and tagging Docker image [asri-web:latest]..."
+docker build -t "asri-web:latest" .
 
 echo "🗜️  Step 2: Saving and compressing image to [asri-image.tar] on the fly..."
-docker save "asri:latest" | gzip > "asri-image.tar"
+docker save "asri-web:latest" | gzip > "asri-web-image.tar"
 
-echo "✅ Done! Your image is dumped at: $(pwd)/asri-image.tar"
+echo "✅ Done! Your image is dumped at: $(pwd)/asri-web-image.tar"
---
+++ insturmentation.ts
@@ -0,0 +1,11 @@
+// instrumentation.ts
+import { registerOTel } from '@vercel/otel';
+
+export function register() {
+  registerOTel({
+    serviceName: 'asri-web',
+    attributes: {
+      'deployment.environment': process.env.NODE_ENV || 'production',
+    },
+  });
+}
